14ae8ded54c472b98b9a6b54b1673f182bcccf74
anhtu <anhtu26091998@gmail.com>
2019-05-21 19:23:23 +0700

    jjn

--- index.php
+++ index.php
@@ -1,2 +1,28 @@
-<?php
-phpinfo();
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<h1>Tính chỉ số BMI</h1>
+<form name="bmi" action="" method="post" >
+    <div class="form-group">
+        <label >Chiều cao</label>
+        <input type="number" placeholder="heght">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label >Cân nặng</label>
+        <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Cân nặng">
+    </div>
+
+    <button type="submit" class="btn btn-primary">BMI</button>
+</form>
+</body>
+</html>
