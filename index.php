ed790e1f1c82acb5a319e7076aa5af4a8c6b298a
anhtu <anhtu26091998@gmail.com>
2019-05-21 19:25:14 +0700

    jjn

--- index.php
+++ index.php
@@ -15,11 +15,10 @@
     <div class="form-group">
         <label >Chiều cao</label>
         <input type="number" placeholder="heght">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
     </div>
     <div class="form-group">
         <label >Cân nặng</label>
-        <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Cân nặng">
+        <input type="number" placeholder="Cân nặng">
     </div>
 
     <button type="submit" class="btn btn-primary">BMI</button>
